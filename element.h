cd4d60cb6c9bf0318485ed7ecd66c4ddee147489
mohamadoueidat <55493203+mohamadoueidat@users.noreply.github.com>
2022-06-17 18:13:55 +0200

    Rendre générique la classe element

--- element.h
+++ element.h
@@ -1,13 +1,14 @@
 #ifndef element_H
 #define element_H
 
+template<typename T>
 class element {
-    double _value;
+    T _value;
     element *_prev;
     element *_next;
 
 public:
-    element(double v=0.0, element *p=nullptr, element *n=nullptr):
+    element(T v=0.0, element *p=nullptr, element *n=nullptr):
         _value(v), _prev(p), _next(n) {}
     ~element() {}
 
@@ -15,7 +16,7 @@ public:
     void set_next(element *n) {_next = n;}
     element *get_prev() {return _prev;}
     void set_prev(element *n) {_prev = n;}
-    double get_value()const {return _value;}
-    void set_value(double v) {_value = v;}
+    T get_value()const {return _value;}
+    void set_value(T v) {_value = v;}
 };
 #endif //element_H
